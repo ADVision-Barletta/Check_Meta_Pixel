cfbc3c080c3e7b668021d854fa081c8ea9917bf6
francescodicandia <francesco.dicandia@gmail.com>
2026-05-28 17:54:32 +0200

    auto: update

--- check-pixel.js
+++ check-pixel.js
@@ -1,5 +1,4 @@
 import { accessSync, readFileSync, writeFileSync, appendFileSync, existsSync, mkdirSync } from 'node:fs';
-import { F_OK } from 'node:fs';
 import { join, dirname } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { resolve4 } from 'node:dns/promises';
